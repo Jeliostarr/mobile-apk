efeb3629822810615569a7b7295f072d1f6bce2a
Jelio Starr Dev <jeliostarrdev@gmail.com>
2026-08-17 15:40:04 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,6 +126,10 @@ dependencies {
   implementation(libs.okhttp)
   // implementation(libs.play.services.location)
   implementation(libs.retrofit)
+  
+  // Fix for lint error: ensure Fragment version >= 1.3.0
+  implementation("androidx.fragment:fragment:1.6.2")
+
   testImplementation(libs.androidx.compose.ui.test.junit4)
   testImplementation(libs.androidx.core)
   testImplementation(libs.androidx.junit)
